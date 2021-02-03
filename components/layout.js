00da6b969c4a8bb36e7896ff5538b12f917dfadc
w7xg <xsxg123456@outlook.com>
2021-02-03 17:13:52 +0800

    update

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'John Andrew'
-export const siteTitle = 'Maga 2024'
+export const siteTitle = 'Make America Great Again --2024'
 
 export default function Layout({ children, home }) {
   return (
